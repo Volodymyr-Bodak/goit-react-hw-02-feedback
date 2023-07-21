08f45ef41ca69d020a9ebfda06f691cf14928a3c
Volodymyr-Bodak <122619914+Volodymyr-Bodak@users.noreply.github.com>
2023-07-21 23:05:11 +0200

    css

--- src/components/Feedback/FeedbackOptions.js
+++ src/components/Feedback/FeedbackOptions.js
@@ -1,14 +1,21 @@
 import React from "react";
+import styles from "components/Feedback/Feedback.module.css";
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-    return (
-      <div>
-        {options.map((option) => (
-          <button key={option} type="button" onClick={() => onLeaveFeedback(option)}>
-            {option}
-          </button>
-        ))}
-      </div>
-    );
-  };
-  export default FeedbackOptions
+  return (
+    <div>
+      {options.map((option) => (
+        <button
+          key={option}
+          type="button"
+          onClick={() => onLeaveFeedback(option)}
+          className={styles["feedback-button"]} // Use the CSS Module class name here
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default FeedbackOptions;
--- src/components/Feedback/Section.js
+++ src/components/Feedback/Section.js
@@ -1,11 +1,12 @@
 
 import React from "react";
+import styles from 'components/Feedback/Section.module.css'
 
 const Section = ({ title, children }) => {
   return (
-    <div>
-      <h2>{title}</h2>
-      {children}
+    <div className={styles.section}>
+      <h2 className={styles.title}>{title}</h2>
+      <div className="section-content">{children}</div>
     </div>
   );
 };
